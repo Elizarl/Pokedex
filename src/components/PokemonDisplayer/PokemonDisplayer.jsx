5d9b7905f5ec52d18ce14f90aa1c60448ca9fc9f
Paola Ramirez <paolaramirez@192.168.1.11>
2023-09-08 12:55:35 -0600

    style: button

--- src/components/PokemonDisplayer/PokemonDisplayer.jsx
+++ src/components/PokemonDisplayer/PokemonDisplayer.jsx
@@ -1,42 +1,35 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React, { useState, useEffect } from "react";
 
-
 import "../PokemonDisplayer/PokemonDisplayer.scss";
 import PokemonDisplayerUI from "./PokemonDisplayerUI";
 
 const PokemonDisplayer = () => {
   const [pokemonData, setPokemonData] = useState();
- 
-  
+
   const pokeNumber = Math.floor(Math.random() * 1011);
 
   useEffect(() => {
-      fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNumber}`)
-       .then((res) => res.json())
-       .then((data) => setPokemonData(data))
-       .catch((error) => console.log(error))
-   }, []);
- 
-  useEffect(() => {
-   const interval= setInterval(async ()=>{
-      await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNumber}`)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNumber}`)
       .then((res) => res.json())
       .then((data) => setPokemonData(data))
-      .catch((error) => console.log(error))
-    },30000);
+      .catch((error) => console.log(error));
+  }, []);
 
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNumber}`)
+        .then((res) => res.json())
+        .then((data) => setPokemonData(data))
+        .catch((error) => console.log(error));
+    }, 30000);
 
-	return () => {
-		clearInterval(interval);
-	};
-   
+    return () => {
+      clearInterval(interval);
+    };
   }, [pokeNumber]);
 
-
-  return (
-   <PokemonDisplayerUI pokemonData={pokemonData}/>
-  );
+  return <PokemonDisplayerUI pokemonData={pokemonData} />;
 };
 
 export default PokemonDisplayer;
--- src/components/PokemonDisplayer/PokemonDisplayerUI.jsx
+++ src/components/PokemonDisplayer/PokemonDisplayerUI.jsx
@@ -1,18 +1,24 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
-import { Button } from '@mui/material';
-
+import { Button } from "@mui/material";
 
 import "../PokemonDisplayer/PokemonDisplayer.scss";
 
-const PokemonDisplayerUI = ({pokemonData}) => (
+const PokemonDisplayerUI = ({ pokemonData }) => (
   <div className="container">
-  <header className="head"><img src="Pokedex_logo.png" /></header>
-  <div className="poke-display">
-    <img src={pokemonData?.sprites?.front_default} />
+    <header className="head">
+      <img src="Pokedex_logo.png" />
+    </header>
+    <div className="poke-display">
+      <img src={pokemonData?.sprites?.front_default} />
+    </div>
+    <Button>
+      <div className="next">
+        <p>Next Pokémon </p>
+        <img src="pokeball.png" />{" "}
+      </div>
+    </Button>
   </div>
-  <Button><div className="next"><p>Next Pokémon </p><img src="pokeball.png" /> </div></Button>
-</div>
-)
+);
 
 export default PokemonDisplayerUI;
